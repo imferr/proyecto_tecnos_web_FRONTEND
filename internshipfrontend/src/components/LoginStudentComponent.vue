b0f3a87542a9d47b08b11b12e693dc37c8b565a6
Kira2602 <misa26amane@gmail.com>
2023-10-16 09:31:05 -0400

    HomePage y RegistroEstudiante

--- internshipfrontend/src/components/LoginStudentComponent.vue
+++ internshipfrontend/src/components/LoginStudentComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <link rel="stylesheet" href="@/assets/style_LoginStudent.css">
+  <div class="componentRegistroStudent">
   <div class="container">
     <div class="title">
       <img src="../assets/logo_Universidad.png" alt="Logo" class="logo">REGISTRARSE
@@ -65,9 +65,190 @@
       </div>
     </form>
   </div>
+</div>
 </template>
 
-<style src="@/assets/style_LoginStudent.css"></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+:root {
+  --main-blue: #4c64b4;
+  --main-yellow: #b6a358;
+  --main-grey: #dddada;
+  --sub-grey: #b8b0b0;
+  --main-black: #111111;
+}
+
+.container .title .logo {
+  height: 70px; 
+  width: auto;  
+  margin-right: 10px; 
+}
+.componentRegistroStudent{
+  display: flex;
+  height: 100vh;
+  justify-content: center; /*verticalmente */
+  align-items: center; /* horizontalmente */
+  padding: 10px;
+  background-image: url('../assets/fondo_principal.jpeg'); 
+  background-size: cover; 
+  background-position: center; 
+  background-repeat: no-repeat;
+}
+
+
+/* contenedores y formas */
+.container {
+  max-width: 900px;
+  width: 100%;
+  background: #ffffffc9;
+  padding: 25px 30px;
+  border-radius: 20px;
+}
+.container .title {
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.container form{
+  position: relative;
+}
+
+.container form .user__details {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+}
+/* inside the form user details */
+form .user__details .input__box {
+  width: calc(100% / 2 - 20px);
+  margin-bottom: 15px;
+}
+
+.user__details .input__box .details {
+  font-weight: 500;
+  margin-bottom: 5px;
+  display: block;
+}
+.user__details .input__box input {
+  height: 40px;
+  width: 100%;
+  outline: none;
+  border-radius: 20px; 
+  border: 1px solid var(--main-grey);
+  padding-left: 15px;
+  font-size: 16px;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+}
+
+.user__details .input__box input:focus,
+.user__details .input__box input:valid {
+  border-color: var(--main-purple);
+}
+
+/* detalles para el genero */
+
+form .gender__details .gender__title {
+  font-size: 20px;
+  font-weight: 500;
+}
+
+form .gender__details .category {
+  display: flex;
+  width: 15%;
+  margin: 15px 0;
+  justify-content: space-between;
+}
+
+.gender__details .category label {
+  display: flex;
+  align-items: center;
+}
+
+.gender__details .category .dot {
+  height: 18px;
+  width: 18px;
+  background: var(--sub-grey);
+  border-radius: 50%;
+  margin: 10px;
+  border: 5px solid transparent;
+  transition: all 0.3s ease;
+}
+
+#dot-1:checked ~ .category .one,
+#dot-2:checked ~ .category .two,
+#dot-3:checked ~ .category .three {
+  border-color: var(--sub-grey);
+  background: var(--main-black);
+}
+
+form input[type="radio"] {
+  display: none;
+}
+
+/* submit button */
+form .button {
+  position: absolute;
+  bottom: 0;
+  right: 10px;
+  height: 30px; 
+  margin: 0;
+}
+
+form .button button {
+  height: 100%;
+  display: inline-block; 
+  padding: 0 25px; 
+  outline: none;
+  color: #fff;
+  border: none;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 20px;
+  background-color:  #4c64b4 ;
+  transition: all 0.3s ease;
+}
+
+form .button button:hover {
+  background: linear-gradient(-135deg, var(--main-yellow), var(--main-blue));
+}
+
+@media only screen and (max-width: 584px) {
+  .container {
+    max-width: 100%;
+  }
+
+  form .user__details .input__box {
+    margin-bottom: 15px;
+    width: 100%;
+  }cd
+
+  form .gender__details .category {
+    width: 100%;
+  }
+
+  .container form .user__details {
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+
+  .user__details::-webkit-scrollbar {
+    width: 0;
+  }
+}
+</style>
+
 
 
 <script>
--- internshipfrontend/src/router/router.js
+++ internshipfrontend/src/router/router.js
@@ -4,7 +4,7 @@ import LoginStudentComponent from '../components/LoginStudentComponent.vue';
 
 
 const routes = [
-    { path : '/', component: LoginStudentComponent, name: 'login' },
+    { path : '/', component: LoginStudentComponent, name: 'registro' },
     { path : '/home', component: HomePageComponent, name: 'home' },
 ];
 
