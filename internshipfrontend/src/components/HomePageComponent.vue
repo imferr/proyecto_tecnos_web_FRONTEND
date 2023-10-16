d786a9aafb456f4bf7decd57b134777855df09ea
Kira2602 <misa26amane@gmail.com>
2023-10-15 23:48:14 -0400

    actualizacion homepage y registro estudiante

--- internshipfrontend/src/components/HomePageComponent.vue
+++ internshipfrontend/src/components/HomePageComponent.vue
@@ -1,4 +1,7 @@
 <template>
+    <!-- En tu componente principal o archivo HTML -->
+<link rel="stylesheet" href="@/assets/style_HomePage.css">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +10,42 @@
     <body>
       
         <section>
-           <div class="carousel-container">
-      <div class="carousel-slide">
-        <img src="https://www.ucb.edu.bo/wp-content/uploads/2023/06/UCB-MUNDO2023.jpg" alt="Image 1">
-      </div>
-      <div class="carousel-slide">
-        <img src="https://www.ucb.edu.bo/wp-content/uploads/2020/05/Walikis-min.jpg" alt="Image 2">
-      </div>
-      <div class="carousel-slide">
-        <img src="image3.jpg" alt="Image 3">
-      </div>
-    </div>
+          <div class="slide">
+                <div class="slide-inner">
+                    <input class="slide-open" type="radio" id="slide-1" 
+                          name="slide" aria-hidden="true" hidden="" checked="checked">
+                    <div class="slide-item">
+                        <img src="https://www.ucb.edu.bo/wp-content/uploads/2023/06/UCB-MUNDO2023.jpg">
+                    </div>
+                    <input class="slide-open" type="radio" id="slide-2" 
+                          name="slide" aria-hidden="true" hidden="">
+                    <div class="slide-item">
+                        <img src="https://www.ucb.edu.bo/wp-content/uploads/2023/05/BANNER-WEB-.jpg">
+                    </div>
+                    <input class="slide-open" type="radio" id="slide-3" 
+                          name="slide" aria-hidden="true" hidden="">
+                    <div class="slide-item">
+                        <img src="https://visitworld.today/media/blog/previews/LmwUcjsO8NreM5VBQYifywq1kGJYMXrupH4rjo1n.jpg">
+                    </div>
+                    <label for="slide-3" class="slide-control prev control-1">‹</label>
+                    <label for="slide-2" class="slide-control next control-1">›</label>
+                    <label for="slide-1" class="slide-control prev control-2">‹</label>
+                    <label for="slide-3" class="slide-control next control-2">›</label>
+                    <label for="slide-2" class="slide-control prev control-3">‹</label>
+                    <label for="slide-1" class="slide-control next control-3">›</label>
+                    <ol class="slide-indicador">
+                        <li>
+                            <label for="slide-1" class="slide-circulo">•</label>
+                        </li>
+                        <li>
+                            <label for="slide-2" class="slide-circulo">•</label>
+                        </li>
+                        <li>
+                            <label for="slide-3" class="slide-circulo">•</label>
+                        </li>
+                    </ol>
+                </div>
+            </div>
         </section>
           <ul>
         
@@ -83,325 +111,8 @@
     </body>
     </template>
     
-    <style>
-            body {
-                font-family: Arial, sans-serif;
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-            }
     
-            header, section, footer {
-                padding: 20px;
-                text-align: center;
-            }
-    
-            .carousel-container {
-      display: flex;
-      overflow: hidden;
-      width: 100%;
-    }
-    
-    .carousel-slide {
-      min-width: 100%;
-      transition: transform 0.5s ease-in-out;
-    }
-    
-    img {
-      width: 100%;
-      height: auto;
-    }
-    
-            .card-container {
-                display: flex;
-                justify-content: space-around;
-                flex-wrap: wrap;
-            }
-    @import url(https://fonts.googleapis.com/css?family=Raleway:400,600,700,800);
-    @import url(https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css);
-    figure.snip1208 {
-      font-family: 'Raleway', Arial, sans-serif;
-      color: #fff;
-      position: relative;
-      overflow: hidden;
-      margin: 10px;
-      min-width: 220px;
-      max-width: 310px;
-      width: 100%;
-      background-color: #ffffff;
-      color: #000000;
-      text-align: left;
-      font-size: 16px;
-    }
-    figure.snip1208 * {
-      -webkit-box-sizing: border-box;
-      box-sizing: border-box;
-      -webkit-transition: all 0.2s ease;
-      transition: all 0.2s ease;
-    }
-    figure.snip1208 img {
-      max-width: 100%;
-      vertical-align: top;
-      -webkit-transform-origin: 50% 100%;
-      transform-origin: 50% 100%;
-      -webkit-transition: all 0.4s ease;
-      transition: all 0.4s ease;
-    }
-    figure.snip1208 figcaption {
-      padding: 25px;
-      position: relative;
-    }
-    
-    figure.snip1208 h3,
-    figure.snip1208 p {
-      margin: 0;
-      padding: 0;
-    }
-    figure.snip1208 h3 {
-      margin-bottom: 10px;
-      display: inline-block;
-      font-weight: 600;
-      color: #333333;
-      text-transform: uppercase;
-    }
-    figure.snip1208 p {
-      font-size: 0.8em;
-      margin-bottom: 20px;
-      line-height: 1.6em;
-    }
-    figure.snip1208 button {
-      border: medium none;
-      padding: 10px 20px;
-      background-color: #4c64b4;
-      font-weight: 800;
-      color: #ffffff;
-      letter-spacing: 2px;
-      text-transform: uppercase;
-      font-size: 0.8em;
-    }
-    figure.snip1208 a {
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      position: absolute;
-      z-index: 1;
-    }
-    figure.snip1208:hover img,
-    figure.snip1208.hover img {
-      -webkit-transform: scale(1.1);
-      transform: scale(1.1);
-    }
-    figure.snip1208:hover .date,
-    figure.snip1208.hover .date {
-      -webkit-transform: rotateY(90deg);
-      transform: rotateY(90deg);
-      -webkit-transition-delay: 0s;
-      transition-delay: 0s;
-    }
-    figure.snip1208:hover i,
-    figure.snip1208.hover i {
-      -webkit-transform: rotateY(0);
-      transform: rotateY(0);
-      -webkit-transition-delay: 0.2s;
-      transition-delay: 0.2s;
-    }
-    figure.snip1208:hover button,
-    figure.snip1208.hover button {
-      background-color: #117964;
-    }
-    
-     footer {
-                background-color: #4c64b4;
-                color: #fff;
-            }
-    
-    
-            
-    </style>
-    <style scoped>
-    .card {
-      box-shadow: 0 0px 1px rgba(0, 0, 0, 0.10), 0 2px 2px rgba(0, 0, 0, 0.10);
-      background: #fff;
-      border-radius: 5px;
-      display: flex;
-      flex-direction: column;
-    }
-    
-    .card-body {
-      display: flex;
-      flex-flow: row wrap;
-      padding: 30px;
-    }
-    
-    header {
-      flex: 100%;
-    }
-    
-    .meta {
-      margin-bottom: 22px;
-    }
-    
-    .chips {
-      align-self: flex-end;
-    }
-    
-    .featured-image {
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      border-top-left-radius: 5px;
-      border-top-right-radius: 5px;
-      overflow: hidden;
-      width: 100%;
-      height: 300px; /* only for mobile, vertical cards: flex-direction: column; */
-    }
-    
-    /* Horizontal kicks in */
-    @media only screen and (min-width: 768px) {
-      .card {
-        flex-direction: row;
-        max-height: 279px; /* desktop horizontal cards with featured image */
-      }
-    
-      .card h3 {
-        font-size: calc(100% + 1vw);
-      }
-    
-      .featured-image {
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-        border-top-right-radius: 0;
-        max-width: 390px;
-        max-height: 279px; /* desktop horizontal cards with featured image */
-      }
-    }
-    
-    /* Monitor screen sizes */
-    @media only screen and (min-width: 1280px) {
-      .card h3 {
-        font-size: 32px;
-      }
-    }
-    
-    /* Typography */
-    * {
-      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-    }
-    
-    h3 {
-      font-size: 32px;
-      line-height: 1.2;
-      font-weight: bold;
-      color: #222;
-      margin: 0.5em 0;
-    }
-    
-    .pre-heading {
-      color: #444;
-      font-size: 20px;
-      font-weight: 400;
-      text-transform: uppercase;
-    }
-    
-    .meta {
-      color: #555;
-      font-size: 16px;
-      text-transform: uppercase;
-    }
-    
-    .author {
-      text-transform: uppercase;
-    }
-    
-    /* NON-CARD STYLES */
-    ul {
-      display: block;
-      margin: 0 auto;
-      max-width: 1160px;
-      padding: 30px;
-    }
-    
-    a {
-      text-decoration: none;
-    }
-    
-    /* Recruitler.com's _chips.scss */
-    /* Container for chips */
-    .chips {
-      white-space: nowrap;
-      overflow-x: auto;
-      -webkit-overflow-scrolling: touch;
-    
-      /* Hide scrollbars Chrome / IE10+ & Edge */
-      &::-webkit-scrollbar {
-        display: none;
-      }
-      -ms-overflow-style: -ms-autohiding-scrollbar;
-      -ms-overflow-style: none;
-    }
-    
-    /* Single chips */
-    .chip {
-      display: inline-block;
-      position: relative;
-      font-size: 16px;
-      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-      line-height: 1.4;
-      white-space: nowrap;
-      color: white;
-      background: #009DFF;
-      border-radius: 25px;
-      margin-right: 8px;
-      padding: 5px 12px;
-      max-height: 32px;
-    
-      &.large {
-        text-transform: uppercase;
-        color: black;
-        border: 1px solid #E0E0E0;
-        background: white;
-        padding: 10px 15px;
-        max-height: 44px;
-      }
-    
-      input {
-        margin-bottom: 0 !important;
-        height: 22px !important;
-        background-color: transparent !important;
-        padding: 3px 0 0 0 !important;
-    
-        &::placeholder {
-          color: black;
-          text-transform: uppercase;
-          font-size: 16px;
-          font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        }
-      }
-    }
-    
-    a.chip {
-      cursor: pointer;
-    
-      &:hover,
-      &:visited,
-      &:active {
-        color: white;
-      }
-    }
-    
-    a.chip.large {
-      &:visited,
-      &:hover,
-      &:active {
-        color: black;
-      }
-    }
-    </style>
-    
-    
-    
-    
+    <style src="@/assets/style_HomePage.css"></style>
     
     <script>
 
