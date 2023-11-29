e97d7290e2e0e40b25ff947cbdf9b2b0b47f79d3
Rosario Calisaya <125493619+Kira2602@users.noreply.github.com>
2023-11-29 15:44:22 -0400

    Create RegistroUsuarioAPI.js

---
+++ internshipfrontend/src/services/RegistroUsuarioAPI.js
@@ -0,0 +1,37 @@
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            error: '',
+        }
+    },
+    methods: {
+        /*SOLO ES UN EJEMPLO, NO ES LA SOLUCION*/
+        /*login() {
+            const student = {
+                email: this.email,
+                password: this.password,
+            };
+            axios.post('http://localhost:3000/api/student/login', student)
+                .then((response) => {
+                    localStorage.setItem('token', response.data.token);
+                    localStorage.setItem('student', JSON.stringify(response.data.student));
+                    this.$router.push({ name: 'home' });
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        this.error = error.response.data.message;
+                    } else {
+                        this.error = error.message;
+                    }
+                });
+        },*/
+
+
+        /*SOLO ES UN EJEMPLO, NO ES LA SOLUCION*/
+        login(){
+            this.$router.push({ name: 'registroUsuario' });
+        }
+    },
+};
