2f968034e37673efd076a6e20468c51aa5286fe9
Laaauuura <104600276+Laaauuura@users.noreply.github.com>
2023-11-30 01:38:20 -0400

    Arreglos

--- internshipfrontend/src/services/RegistroAdminAPI.js
+++ internshipfrontend/src/services/RegistroAdminAPI.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Swal from "sweetalert2";
 
 
 const API_BASE_URL = 'http://localhost:8080/api/v1/administradores';
@@ -8,19 +7,12 @@ class RegistroAdminAPI {
     async register(adminData) {
         try {
             const response = await axios.post(`${API_BASE_URL}/register`, adminData);
-            Swal.fire({
-              icon: "success",
-              title: "Registro exitoso",
-              text: "El registro se ha completado con éxito.",
-            });
+           
+            console.log("Registro exitoso:", response.data);
             return response.data;
-        } catch (error) {
-          Swal.fire({
-            icon: "error",
-            title: "Error en el registro",
-            text: "Hubo un error al registrar al administrador. Por favor, inténtalo de nuevo.",
-          });
-            throw error;
+      } catch (error) {
+          console.error("Error en el registro:", error);
+          throw error;
             
         }
     }
