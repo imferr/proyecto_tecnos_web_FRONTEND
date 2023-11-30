6c2495da38b41bf9e5ae55b37e1b91e13644c9a2
Laaauuura <104600276+Laaauuura@users.noreply.github.com>
2023-11-30 00:30:47 -0400

    alertas en usuario

--- internshipfrontend/src/services/RegistroUsuarioAPI.js
+++ internshipfrontend/src/services/RegistroUsuarioAPI.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Swal from "sweetalert2";
 
 const API_BASE_URL = 'http://localhost:8080/api/v1/usuario';
 
@@ -6,12 +7,22 @@ class RegistroUsuarioAPI {
     async register(userData) {
         try {
             const response = await axios.post(`${API_BASE_URL}/register`, userData);
-            return response.data;
+            console.log("Registro exitoso:", response.data);
+            Swal.fire({
+                icon: "success",
+                title: "Registro exitoso",
+                text: "El registro se ha completado con éxito.",
+              });
+              return response.data;
         } catch (error) {
-                // Manejo de errores
-                console.log(error);
-            throw error;
-        }
+            console.error("Error en el registro:", error);
+    
+            Swal.fire({
+              icon: "error",
+              title: "Error en el registro",
+              text: "Hubo un error al registrar al administrador. Por favor, inténtalo de nuevo.",
+            });
+          }
     }
 
 }
